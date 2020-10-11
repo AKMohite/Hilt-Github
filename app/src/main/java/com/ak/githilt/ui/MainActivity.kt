72205c28261e9f61eed2e137b73b17ac7a0ab162
Ashish Mohite <ashishmohite1996@gmail.com>
2020-10-11 11:19:01 +0530

    Migrate code from activity to fragment

--- app/src/main/java/com/ak/githilt/ui/MainActivity.kt
+++ app/src/main/java/com/ak/githilt/ui/MainActivity.kt
@@ -21,59 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "MainActivity"
 
-    private val viewModel: RepositoryViewModel by viewModels()
-
-    private val adapter = RepositoryAdapter()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initViews()
-        subscribeObservers()
-        viewModel.setStateEvent(RepoStateEvent.GetRepoEvents("android", 1)) // TODO pagination
-    }
-
-    private fun initViews() {
-        repo_list.layoutManager = LinearLayoutManager(this)
-        repo_list.adapter = adapter
-    }
-
-    private fun subscribeObservers(){
-        viewModel.dataState.observe(this, Observer { dataState ->
-            when(dataState){
-                is DataState.Success<List<Repo>> -> {
-                    displayProgressBar(false)
-                    loadData(dataState.data)
-                }
-
-                is DataState.Error -> {
-                    displayProgressBar(false)
-                    displayError(dataState.exception.message)
-                }
-
-                is DataState.Loading -> {
-                    displayProgressBar(true)
-                }
-            }
-        })
-    }
-
-    private fun displayError(message: String?){
-        Toast.makeText(this, message ?: "Unknown Error", Toast.LENGTH_LONG).show()
-    }
-
-    private fun displayProgressBar(isVisible: Boolean){
-        progress_bar.visibility = if (isVisible) View.VISIBLE else View.GONE
-    }
-
-    private fun loadData(repos: List<Repo>){
-        /*val sb = StringBuilder("")
-        for (repo in repos){
-            sb.append("${repo.id} ==> ${repo.fullName}\n")
-        }
-        text.text = sb.toString()*/
-
-        adapter.updateRepoList(repos)
     }
 }
---
+++ app/src/main/java/com/ak/githilt/ui/home/HomePageFragment.kt
@@ -0,0 +1,91 @@
+package com.ak.githilt.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ak.githilt.R
+import com.ak.githilt.databinding.FragmentHomePageBinding
+import com.ak.githilt.model.Repo
+import com.ak.githilt.ui.RepoStateEvent
+import com.ak.githilt.ui.RepositoryAdapter
+import com.ak.githilt.ui.RepositoryViewModel
+import com.ak.githilt.util.DataState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_home_page.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class HomePageFragment: Fragment() {
+
+    lateinit var binding: FragmentHomePageBinding
+
+    private val viewModel: RepositoryViewModel by viewModels()
+
+    private val repoAdapter = RepositoryAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_page, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        subscribeObservers()
+        viewModel.setStateEvent(RepoStateEvent.GetRepoEvents("android", 1)) // TODO pagination
+    }
+
+    private fun initViews() {
+        repo_list.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = repoAdapter
+        }
+    }
+
+    private fun subscribeObservers(){
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
+            when(dataState){
+                is DataState.Success<List<Repo>> -> {
+                    displayProgressBar(false)
+                    loadData(dataState.data)
+                }
+
+                is DataState.Error -> {
+                    displayProgressBar(false)
+                    displayError(dataState.exception.message)
+                }
+
+                is DataState.Loading -> {
+                    displayProgressBar(true)
+                }
+            }
+        })
+    }
+
+    private fun displayError(message: String?){
+        Toast.makeText(context, message ?: "Unknown Error", Toast.LENGTH_LONG).show()
+    }
+
+    private fun displayProgressBar(isVisible: Boolean){
+        progress_bar.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+    private fun loadData(repos: List<Repo>){
+
+        repoAdapter.updateRepoList(repos)
+    }
+}
